f0435573a4a6f3aad6d4efb8365e73c9ba139e96
Matteo Magni <matteo@magni.me>
2014-09-14 18:32:43 +0200

    added application

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 hoaas
 =====
+
+v0.0.1
+
+HOAAS HOAAS (HOdor As A Service) provides a modern, RESTful, scalable solution to the common problem of talk with Hodor
+![Hodor](https://github.com/ilbonzo/hoaas/raw/master/public/images/talk-hodor.gif "Hodor")
+
+img(src="/images/talk-hodor.gif")
+
+# Content Negotiation
+
+HOAAS will respond to the following 'Accept:' values with appropriate content
+
+* text/plain - Content will be returned as a plain string.
+* application/json - Content will be returned as a JSON object { message: 'message' }
+* text/html - Content will be returned as an HTML page with a twitter bootstrap hero unit, containing the Hodor's message.
+
+# API
+
+## /hodor
+
+Will return content of the form 'Hodor' with or without different final dot.
+
+## /hodor/:number
+Will return content of the form 'Hodor, Hodor' for :number times with or without different final dot.
+
+## /hodor/question/:question
+p Will return content of right Hodor answer for your question.
+
+# Alternative URLs
+
+- http://hoaas.herokuapp.com
+
+
+![Hodor](https://github.com/ilbonzo/hoaas/raw/master/public/images/1-hodor.jpg "Hodor")
+
+All contributions are very welcome.
---
+++ routes/hodor.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    var message = 'Hodor';
+
+    var points = ['!', '.', '?', ' '];
+    var point = points[Math.floor(Math.random() * points.length)];
+    message = message + point;
+
+    res.format({
+        'text/plain': function(){
+            res.send(message);
+        },
+
+        'text/html': function(){
+            res.render('hodor',{ 'message': message });
+        },
+
+        'application/json': function(){
+            res.send({ 'message': message});
+        },
+
+        'default': function() {
+            // log the request and respond with 406
+            res.status(406).send('Not Acceptable');
+        }
+    });
+});
+
+router.get('/number/:number', function(req, res) {
+    var message = '';
+    for (i = 0; i < req.params.number; i++ ) {
+        message += 'Hodor, ';
+    }
+    message = message.slice(0, -2);
+
+    var points = ['!', '.', '?', ' '];
+    var point = points[Math.floor(Math.random() * points.length)];
+    message = message + point;
+
+    res.format({
+        'text/plain': function(){
+            res.send(message);
+        },
+
+        'text/html': function(){
+            res.render('hodor',{ 'message': message });
+        },
+
+        'application/json': function(){
+            res.send({ 'message': message});
+        },
+
+        'default': function() {
+            // log the request and respond with 406
+            res.status(406).send('Not Acceptable');
+        }
+    });
+});
+
+router.get('/question/:question', function(req, res) {
+    var message = '';
+
+    var number = Math.floor((Math.random() * 10) + 1);
+
+    for (i = 0; i < number; i++ ) {
+        message += 'Hodor, ';
+    }
+    message = message.slice(0, -2);
+
+    var points = ['!', '.', '?', ' '];
+    var point = points[Math.floor(Math.random() * points.length)];
+
+    message = message + point;
+
+    res.format({
+        'text/plain': function(){
+            res.send(message);
+        },
+
+        'text/html': function(){
+            res.render('Hodor',{ 'message': message });
+        },
+
+        'application/json': function(){
+            res.send({ 'message': message});
+        },
+
+        'default': function() {
+            // log the request and respond with 406
+            res.status(406).send('Not Acceptable');
+        }
+    });
+});
+
+module.exports = router;
